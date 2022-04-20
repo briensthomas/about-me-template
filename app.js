7317574dc2d7827f510c274b6d9cf7902df6786c
Brien Thomas <thomasbrien94@gmail.com>
2022-04-20 15:14:14 -0700

    Added image to assets, source linked image for favorite animal, set class as hidden, added event listener(click) to remove hidden and show favorite animal.

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 // import functions
 
 // grab DOM elements
-
+const animalButton = document.getElementById('animalButton');
+const animalDiv = document.getElementById('animalDiv');
 // set event listeners 
     // get user input
     // use user input to update state 
     // update DOM to reflect the new state
+animalButton.addEventListener('click', () => {
+    animalDiv.classList.remove('hidden');
+});
